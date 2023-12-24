3cef1f32c260ab42eee3ee96adec483dd7f08d60
Anand Singh <43192082+the-anand-singh@users.noreply.github.com>
2023-12-24 11:28:23 +0530

    tictactoe

---
+++ script.js
@@ -0,0 +1,48 @@
+
+let boxes = document.querySelectorAll(".box");
+let reset_btn = document.querySelector(".reset");
+// console.log(boxes);
+let turn0 = true;
+
+const winpatterns = [
+    [0,1,2],
+    [0,4,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click",() => {
+        console.log("box was clicked");
+        if(turn0 === true){
+            box.innerText = "O";
+            turn0 = false;
+        }
+        else{
+            box.innerText = "X";
+            turn0 = true;
+        }
+        box.disabled = true;
+        checkWinner();
+    })
+})
+
+const checkWinner = () => {
+    for(let pattern of winpatterns){
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+
+        if(pos1val!="" && pos2val!="" && pos3val!=""){
+            if(pos1val==pos2val && pos2val==pos3val){
+                alert("Winner is: " +pos1val);
+                // alert(pos1val);
+                // console.log("Winner",pos1val);
+            }
+        }
+    }
+};
